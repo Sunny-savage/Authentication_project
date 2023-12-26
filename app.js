8186219a914c4886a29adff54961239c8e00b84b
Sunny-savage <vishal200231@gmail.com>
2023-12-26 19:45:13 +0530

    another try

--- app.js
+++ app.js
@@ -20,12 +20,7 @@ app.use(
     credentials: true,
   })
 );
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+
 // using routes
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/task", taskRouter);
