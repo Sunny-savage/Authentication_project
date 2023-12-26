b3f86a8207d0659c92091d0af4100588a3a2f88e
Sunny-savage <vishal200231@gmail.com>
2023-12-26 13:41:00 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import { config } from "dotenv";
+import userRouter from "./routes/user.js";
+import taskRouter from "./routes/task.js";
+import cookieParser from "cookie-parser";
+import { errorMiddleware } from "./middleware/error.js";
+import cors from "cors";
+export const app = express();
+config({
+  path: "./data/config.env",
+});
+
+//middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+// using routes
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/task", taskRouter);
+
+app.get("/", (req, res) => {
+  res.send("nice");
+});
+
+//Using error middleware
+app.use(errorMiddleware);
